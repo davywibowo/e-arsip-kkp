97b40213d2a6507bb5e443baf936846f11691208
Glenn-Rhee <glennviktor5@gmail.com>
2025-10-09 14:29:46 +0700

    refactor: made sidebar dynamic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { AppSidebar } from "@/components/AppSidebar";
+import { DataUser, ResponsePayload } from "@/types";
+import { cookies } from "next/headers";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { SiteHeader } from "@/components/site-header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,18 +23,53 @@ export const metadata: Metadata = {
   description: "employes electronic archive ",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
+
+  const baseUrl =
+    process.env.NODE_ENV === "production"
+      ? "https://e-arsip-kkp.vercel.app"
+      : "http://localhost:3000";
+
+  const response = await fetch(baseUrl + "/api/user", {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const dataResponse =
+    (await response.json()) as ResponsePayload<DataUser | null>;
+
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "calc(var(--spacing) * 72)",
+              "--header-height": "calc(var(--spacing) * 12)",
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar
+            variant="inset"
+            dataUser={
+              dataResponse.status === "failed" ? null : dataResponse.data!
+            }
+          />
+          <SidebarInset>
+            <SiteHeader token={token} />
+            {children}
+          </SidebarInset>
+        </SidebarProvider>
         <Toaster />
-        {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,17 @@
 import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
-import { SiteHeader } from "@/components/site-header";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import data from "@/util/data.json";
-import { AppSidebar } from "@/components/AppSidebar";
-import { cookies } from "next/headers";
-import { DataUser, ResponsePayload } from "@/types";
 
 export default async function Page() {
-  const cookieStore = await cookies();
-  const token = cookieStore.get("token")?.value;
-  const baseUrl =
-    process.env.NODE_ENV === "production"
-      ? "https://e-arsip-kkp.vercel.app"
-      : "http://localhost:3000";
-
-  const response = await fetch(baseUrl + "/api/user", {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  const dataResponse =
-    (await response.json()) as ResponsePayload<DataUser | null>;
   return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar
-        variant="inset"
-        dataUser={dataResponse.status === "failed" ? null : dataResponse.data!}
-      />
-      <SidebarInset>
-        <SiteHeader token={token} />
-        <div className="flex flex-1 flex-col">
-          <div className="@container/main flex flex-1 flex-col gap-2">
-            <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
+    <div className="flex flex-1 flex-col">
+      <div className="@container/main flex flex-1 flex-col gap-2">
+        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+          <SectionCards />
 
-              <DataTable data={data} />
-            </div>
-          </div>
+          <DataTable data={data} />
         </div>
-      </SidebarInset>
-    </SidebarProvider>
+      </div>
+    </div>
   );
 }
