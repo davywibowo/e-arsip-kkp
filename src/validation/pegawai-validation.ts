14e2cf33176f8e8922cf67f668626aa222879b38
Glenn-Rhee <glennviktor5@gmail.com>
2025-10-17 18:36:57 +0700

    fix: fixed type data of nipBaru and nipLama. now user can empty nip if add '-' at field

--- src/validation/pegawai-validation.ts
+++ src/validation/pegawai-validation.ts
@@ -5,16 +5,22 @@ export default class PegawaiValidation {
     namaPegawai: z
       .string({ error: "Please fill 'Nama Pegawai' properly!" })
       .min(1, { error: "Minimum length of Nama Pegawai is 1" }),
-    nipLama: z
-      .string({ error: "Please fill 'NIP Lama' properly!" })
-      .regex(/^\d+$/, { error: "NIP Lama must contain only numbers" })
-      .min(8, { error: "NIP Lama must be exactly 8 characters" })
-      .max(8, { error: "NIP Lama must be exactly 8 characters" }),
-    nipBaru: z
-      .string({ error: "Please fill 'NIP Baru' properly!" })
-      .regex(/^\d+$/, { error: "NIP Baru must contain only numbers" })
-      .min(18, { error: "NIP Baru must be exactly 18 characters" })
-      .max(18, { error: "NIP Baru must be exactly 18 characters" }),
+    nipLama: z.union([
+      z.literal("-"),
+      z
+        .string({ error: "Please fill 'NIP Lama' properly!" })
+        .regex(/^\d+$/, { error: "NIP Lama must contain only numbers" })
+        .min(8, { error: "NIP Lama must be exactly 8 characters" })
+        .max(8, { error: "NIP Lama must be exactly 8 characters" }),
+    ]),
+    nipBaru: z.union([
+      z.literal("-"),
+      z
+        .string({ error: "Please fill 'NIP Baru' properly!" })
+        .regex(/^\d+$/, { error: "NIP Baru must contain only numbers" })
+        .min(18, { error: "NIP Baru must be exactly 18 characters" })
+        .max(18, { error: "NIP Baru must be exactly 18 characters" }),
+    ]),
     noArsip: z
       .string({ error: "Please fill 'No Arsip' properly!" })
       .min(3, { error: "Minimum length of No Arsip is 3" })
@@ -26,16 +32,22 @@ export default class PegawaiValidation {
     namaPegawai: z
       .string({ error: "Please fill 'Nama Pegawai' properly!" })
       .min(1, { error: "Minimum length of Nama Pegawai is 1" }),
-    nipLama: z
-      .string({ error: "Please fill 'NIP Lama' properly!" })
-      .regex(/^\d+$/, { error: "NIP Lama must contain only numbers" })
-      .min(8, { error: "NIP Lama must be exactly 8 characters" })
-      .max(8, { error: "NIP Lama must be exactly 8 characters" }),
-    nipBaru: z
-      .string({ error: "Please fill 'NIP Baru' properly!" })
-      .regex(/^\d+$/, { error: "NIP Baru must contain only numbers" })
-      .min(18, { error: "NIP Baru must be exactly 18 characters" })
-      .max(18, { error: "NIP Baru must be exactly 18 characters" }),
+    nipLama: z.union([
+      z.literal("-"),
+      z
+        .string({ error: "Please fill 'NIP Lama' properly!" })
+        .regex(/^\d+$/, { error: "NIP Lama must contain only numbers" })
+        .min(8, { error: "NIP Lama must be exactly 8 characters" })
+        .max(8, { error: "NIP Lama must be exactly 8 characters" }),
+    ]),
+    nipBaru: z.union([
+      z.literal("-"),
+      z
+        .string({ error: "Please fill 'NIP Baru' properly!" })
+        .regex(/^\d+$/, { error: "NIP Baru must contain only numbers" })
+        .min(18, { error: "NIP Baru must be exactly 18 characters" })
+        .max(18, { error: "NIP Baru must be exactly 18 characters" }),
+    ]),
     noArsip: z
       .string({ error: "Please fill 'No Arsip' properly!" })
       .min(3, { error: "Minimum length of No Arsip is 3" })
