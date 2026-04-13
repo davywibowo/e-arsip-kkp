4e9e2d9fcfc948134e941100e7b4ba7649c66dcb
Glenn-Rhee <glennviktor5@gmail.com>
2026-04-14 02:19:30 +0700

    feat: added proxy file

---
+++ src/proxy.ts
@@ -0,0 +1,70 @@
+import { cookies } from "next/headers";
+import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
+import { ResponsePayload } from "./types";
+
+export async function proxy(req: NextRequest) {
+  const cookieStore = await cookies();
+  const url = req.nextUrl.pathname;
+  const tokenCookie = cookieStore.get("token");
+  const token = tokenCookie ? tokenCookie.value : null;
+
+  if (url.includes("/api")) {
+    if (url.includes("/auth")) {
+      if (req.method === "DELETE") {
+        if (!token) {
+          return NextResponse.json<ResponsePayload>({
+            status: "failed",
+            statusCode: 403,
+            message: "Oops! You already logout!",
+          });
+        }
+      } else {
+        if (token) {
+          return NextResponse.json<ResponsePayload>({
+            status: "failed",
+            statusCode: 403,
+            message: "You have been loged in!",
+          });
+        }
+      }
+    }
+
+    if (url.includes("/pegawai")) {
+      if (req.method !== "GET" && !token) {
+        return NextResponse.json<ResponsePayload>({
+          message: "Unathorized! Login first!",
+          statusCode: 403,
+          status: "failed",
+        });
+      }
+    }
+
+    if (url.includes("/user")) {
+      if (req.method !== "GET" && !token) {
+        return NextResponse.json<ResponsePayload>({
+          status: "failed",
+          message: "Oops! You don't have any access for this!",
+          statusCode: 403,
+        });
+      }
+    }
+  } else {
+    if (url.includes("/auth")) {
+      if (token) {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+    }
+
+    if (url.includes("/manajemen-pengguna")) {
+      if (!token) {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const matcher: MiddlewareConfig = {
+  matcher: ["/:path"],
+};
